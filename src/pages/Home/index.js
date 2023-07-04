f45fbf3a9a343c5df34d65b678e083eb7034dc5b
Walter Fabri <walterfabri18@gmail.com>
2023-07-04 15:31:20 -0300

    created loading and created not found page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,6 +9,7 @@ import './home.css'
 function Home() {
 
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
 
@@ -22,11 +23,20 @@ function Home() {
             })
 
             setFilmes(response.data.results.slice(0, 10));
+            setLoading(false);
         }
 
         loadFilmes();
 
     }, [])
+
+    if(loading) {
+        return(
+            <div className='loading'>
+                <h2>Carregando filmes...</h2>
+            </div>
+        );
+    }
 
     return(
         <div className='container'>
