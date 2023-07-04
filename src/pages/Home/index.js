fbf1cde1c5ef3ae82ed2f7647a116ff257b0f036
Walter Fabri <walterfabri18@gmail.com>
2023-07-04 10:30:29 -0300

    listed movies on the home screen

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
 import api from '../../services/api'
+import { Link } from 'react-router-dom';
+
+import './home.css'
 
 //URL DA API: /movie/now_playing?api_key=e0e3c742e23c07da03302daefe5e4eca&language=pt-BR
 
@@ -18,17 +21,26 @@ function Home() {
                 }
             })
 
-            console.log(response.data.results);
+            setFilmes(response.data.results.slice(0, 10));
         }
 
         loadFilmes();
 
-
     }, [])
 
     return(
-        <div>
-            <h1>Bem-vindo à home.</h1>
+        <div className='container'>
+            <div className='lista-filmes'>
+                {filmes.map((filme) => {
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}></img>
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    );
+                })}
+            </div>
         </div>
     );
 }
